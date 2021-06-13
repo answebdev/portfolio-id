7082dc604118ea3fd2d7d79de02d7aa6d7ad9bce
ANS <36783010+answebdev@users.noreply.github.com>
2021-06-12 22:11:49 -0700

    clean up code

--- src/components/layout/BackButton.js
+++ src/components/layout/BackButton.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-
 import classes from '../../styles/BackButton.module.css';
 
 const BackButton = () => {
   const history = useHistory();
+
   return (
     <div>
       <button
         onClick={() => history.goBack()}
         className={classes.BackBtn}
         variant='primary'
-        style={{ marginRight: '5px' }}
       >
         <i className='fas fa-arrow-left'></i>&nbsp;BACK
       </button>
